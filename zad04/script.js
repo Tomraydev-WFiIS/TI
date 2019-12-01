841fe878b3d9e782e3dff7a6105cf4e0cf344dba
Tomasz Rajchel <7rajchel@fis.agh.edu.pl>
2019-12-01 23:04:01 +0100

    finish zad04

--- zad04/script.js
+++ zad04/script.js
@@ -1,17 +1,19 @@
-function draw() {
+function draw(data) {
     let canvas = document.getElementById('histogram');
     let ctx = canvas.getContext('2d');
     w = canvas.width;
     h = canvas.height;
     ctx.font = "14px Arial";
+    ctx.clearRect(0, 0, w,h);
 
     let color = ['green', 'yellow', 'orange', 'blue'];
     let courses = ["Bazy danych","Metody numeryczne","Analiza obrazów","Techniki internetowe"];
-    let data = [25, 11, 42, 89]; //dummy
+    let id = ["BD", "MN", "AO", "TI"];
     for (let i = 0; i < 4; i++) {
         ctx.fillStyle = color[i];
-        ctx.fillRect(i*0.25*w, h - 30 - data[i], 0.25*w, data[i]);
-        ctx.fillText(courses[i], i*0.25*w +16, h-10);
+        ctx.fillRect(i*0.25*w, h - 30 - data[id[i]]*5, 0.25*w, data[id[i]]*5);
+        ctx.fillText(courses[i], i*0.25*w +16, h-10); //label
+        ctx.fillText(data[id[i]], i*0.25*w +0.12*w, h - 50 - data[id[i]]*5);// counter
     }
 }
 
@@ -19,19 +21,18 @@ function send(event){
     event.preventDefault(); // prevent page reload
     let choice = document.form1.course.value;    // wczytaj formularz
     let XHR = new XMLHttpRequest();                  // utworz obiekt XHR
-    XHR.open('POST', "../cgi-bin/zad04/update.py", true);  // otworz polaczenie z plikiem python - asynchroniczne
+    XHR.open('POST', "../cgi-bin/zad04/update.py", true);  // otworz polaczenie z plikiem python - asynchroniczne true
     data = 'info=' + encodeURIComponent(choice)
     
-    XHR.onreadystatechange = function (aEvt) {// onreadystatechange . . .
-        // console.log("working");
-        console.log(XHR.readyState);
-        console.log(XHR.status);
+    XHR.onreadystatechange = function (aEvt) {
+        console.log("XHR.readyState: " + XHR.readyState);
+        console.log("XHR.status: " + XHR.status);
         if (XHR.readyState == 4) {
             if (XHR.status == 200)  {
-                console.log(XHR.responseText); //debug
-                let response = JSON.parse(XHR.responseText);   //wczytaj dane
-                console.log(response) //debug
-                draw();   //rysuj histogram
+                console.log("XHR.responseText: " + XHR.responseText);
+                data = JSON.parse(XHR.responseText);   //wczytaj dane
+                console.log("data: " + JSON.stringify(data))
+                draw(data);   //rysuj histogram
             }
         }  
     }
